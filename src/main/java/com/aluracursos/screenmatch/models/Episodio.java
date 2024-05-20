82a11c4268b444e415c51d926a3791a59ceea07e
Hernan <hjviltez@gmail.com>
2024-05-20 16:31:23 -0300

    205.2

---
+++ src/main/java/com/aluracursos/screenmatch/models/Episodio.java
@@ -0,0 +1,79 @@
+package com.aluracursos.screenmatch.models;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+    public class Episodio {
+        private Integer temporada;
+        private  String titulo;
+        private Integer numeroEpisodio;
+        private Double evaluacion;
+        private LocalDate fechaDeLanzamiento;
+
+        public Episodio(Integer temporada, DatosEpisodio datosEpisodio) {
+            this.temporada = temporada;
+            this.titulo = datosEpisodio.titulo();
+            this.numeroEpisodio = datosEpisodio.numeroEpisodio();
+            try {
+                this.evaluacion = Double.valueOf(datosEpisodio.evaluacion()) ;
+            } catch (NumberFormatException ex){
+                this.evaluacion = 0.0;
+            }
+            try{
+                this.fechaDeLanzamiento = LocalDate.parse(datosEpisodio.fechaDeLanzamiento());
+            }catch (DateTimeParseException ex){
+                this.fechaDeLanzamiento = null;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return
+                    "temporada=" + temporada +
+                            ", titutlo='" + titulo + '\'' +
+                            ", numeroEpisodio=" + numeroEpisodio +
+                            ", evaluacion=" + evaluacion +
+                            ", fechaDeLanzamiento=" + fechaDeLanzamiento;
+        }
+
+        public Integer getTemporada() {
+            return temporada;
+        }
+
+        public void setTemporada(Integer temporada) {
+            this.temporada = temporada;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+
+        public Integer getNumeroEpisodio() {
+            return numeroEpisodio;
+        }
+
+        public void setNumeroEpisodio(Integer numeroEpisodio) {
+            this.numeroEpisodio = numeroEpisodio;
+        }
+
+        public Double getEvaluacion() {
+            return evaluacion;
+        }
+
+        public void setEvaluacion(Double evaluacion) {
+            this.evaluacion = evaluacion;
+        }
+
+        public LocalDate getFechaDeLanzamiento() {
+            return fechaDeLanzamiento;
+        }
+
+        public void setFechaDeLanzamiento(LocalDate fechaDeLanzamiento) {
+            this.fechaDeLanzamiento = fechaDeLanzamiento;
+        }
+    }
+
+}
--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -3,24 +3,28 @@ package com.aluracursos.screenmatch.principal;
 import com.aluracursos.screenmatch.models.DatosEpisodio;
 import com.aluracursos.screenmatch.models.DatosSerie;
 import com.aluracursos.screenmatch.models.DatosTemporada;
+import com.aluracursos.screenmatch.models.Episodio;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
-    public class Principal {
+public class Principal {
 
         private Scanner teclado = new Scanner(System.in);
         private final String URL = "https://www.omdbapi.com/?t=";
         private final String APIKEY = "&apikey=e1bc4c5f&";
         private ConsumoAPI consumoAPI = new ConsumoAPI();
         private ConvierteDatos conversor = new ConvierteDatos();
+
         public void muestraElMenu(){
             System.out.println("Escribe el nombre de la série que deseas buscar");
             var nombreSerie = teclado.nextLine();
+
             //obtener la serie
             var json = consumoAPI.obtenerDatos(URL + nombreSerie.replace(" ", "+") + APIKEY);
             DatosSerie datos = null;
@@ -53,6 +57,51 @@ import java.util.Scanner;
             }
             //lambda con clousures
             temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+            //ejemplos de stream-lambda
+            List<String> nombres = Arrays.asList("Genesys","Eric","Maria","Brenda");
+
+            nombres.stream()
+                    .sorted()
+                    .limit(2)
+                    .filter(n -> n.startsWith("E"))
+                    .map(n -> n.toUpperCase())
+                    .forEach(System.out::println);
+
+            List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                    .flatMap(t -> t.episodios().stream())
+                    .collect(Collectors.toList());
+            System.out.println("\n Top 5 episodios");
+
+            datosEpisodios.stream()
+                    .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
+                    .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                    .limit(5)
+                    .forEach(System.out::println);
+
+            List<Episodio> episodios = temporadas.stream()
+                    .flatMap(t -> t.episodios().stream()
+                            .map(d -> new Episodio(t.numero(), d)))
+                    .collect(Collectors.toList());
+
+            episodios.forEach(System.out::println);
+
+            System.out.println("a partir de que año deseas ver los episodios?");
+            var fecha = teclado.nextInt();
+            teclado.nextLine();
+
+            LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            episodios.stream()
+                    .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+                    .forEach(e -> System.out.println(
+                            "Temporada: " + e.getTemporada() +
+                                    " Episodio: " + e.getTitulo() +
+                                    " Fecha de Lanzamiento: " + e.getFechaDeLanzamiento().format(formatter)
+                    ));
+
+        }
         }
     }
 
